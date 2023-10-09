ae7734038af801f85bae12b9904d083d0057db09
Кирилл Рябов <is042s17@oak.ipa.csc.sibsutis.ru>
2023-10-09 22:58:48 +0700

    Update lab-1

--- lab-1/scripts/plot.py
+++ lab-1/scripts/plot.py
@@ -7,13 +7,17 @@ plt.rcParams["figure.figsize"] = (20, 10)
 
 data = pd.read_csv("results.csv")
 
+_, ax = plt.subplots()
 
-plt.plot(data["message_length(mb)"][:7].to_numpy(), data["time"][:7].to_numpy(), c="purple", marker="x")
-plt.plot(data["message_length(mb)"][7:14].to_numpy(), data["time"][7:14].to_numpy(), c="red", marker=".")
-plt.plot(data["message_length(mb)"][14:].to_numpy(), data["time"][14:].to_numpy(), c="blue", marker="P")
 
-plt.grid()
-plt.xticks(data["message_length(mb)"][:7].to_numpy())
+ax.plot(data["message_length(mb)"][:7].to_numpy(), data["time"][:7].to_numpy(), c="purple", marker="x")
+ax.plot(data["message_length(mb)"][7:14].to_numpy(), data["time"][7:14].to_numpy(), c="red", marker=".")
+ax.plot(data["message_length(mb)"][14:].to_numpy(), data["time"][14:].to_numpy(), c="blue", marker="P")
+
+ax.grid()
+ax.set_xscale('log')
+ax.set_xticks(data["message_length(mb)"][:7].to_numpy())
+ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 plt.title("Зависимость времени передачи сообщения от его размера\nна разных уровнях коммуникационной среды")
 plt.legend(["Уровень оперативной памяти NUMA узла", "Уровень внутрисистемной шины Intel QPI,\nобъединяющей процессоры NUMA-узлов", "Уровень сети связи между ЭМ (адаптер InfiniBand QDR)"])
 plt.ylabel("Время передачи сообщения (с)\n")
--- lab-1/src/main.cpp
+++ lab-1/src/main.cpp
@@ -3,32 +3,33 @@
 #include <vector>
 
 int main(int argc, char *argv[]) {
-    int msg_size = 1;
+    std::size_t msg_size = 1024 * 1024;
     if (argc > 2 && std::string_view(argv[1]) == "--msg-size") {
-        msg_size = std::atoi(argv[2]);
+        msg_size *= std::atoi(argv[2]);
     }
     int commsize, rank;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &commsize);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    const auto n = msg_size * 1024 * 1024;
-    std::vector<std::uint8_t> read_buf(n);
-    std::vector<std::uint8_t> send_buf(n, 88);
-    std::vector<MPI_Request> requests(2*commsize);
-    std::vector<MPI_Status> stats(2*commsize);
-    auto i = rank;
+    const std::size_t nruns = 10;
+    std::vector<std::uint8_t> read_buf(msg_size);
+    std::vector<std::uint8_t> send_buf(msg_size, 88);
     auto t = MPI_Wtime();
-    do {
-        MPI_Isend(send_buf.data(), n, MPI_UINT8_T, i, 0, MPI_COMM_WORLD, &requests[i]);
-        MPI_Irecv(read_buf.data(), n, MPI_UINT8_T, i, 0, MPI_COMM_WORLD, &requests[commsize + i]);
-        i = (i + 1) % commsize;
-    } while (i != rank);
-    MPI_Waitall(2*commsize, requests.data(), stats.data());
-    t = MPI_Wtime() - t;
-    double max_time;
-    MPI_Reduce(&t, &max_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    std::vector<MPI_Request> requests(2);
+    std::vector<MPI_Status> stats(2);
+    for (std::size_t i = 0; i < nruns; ++i) {
+        if (rank == 0){
+            MPI_Isend(send_buf.data(), msg_size, MPI_UINT8_T, 1, 0, MPI_COMM_WORLD, &requests[0]);
+            MPI_Irecv(read_buf.data(), msg_size, MPI_UINT8_T, 1, 0, MPI_COMM_WORLD, &requests[1]);
+        } else if (rank == 1) {
+            MPI_Irecv(read_buf.data(), msg_size, MPI_UINT8_T, 0, 0, MPI_COMM_WORLD, &requests[0]);
+            MPI_Isend(send_buf.data(), msg_size, MPI_UINT8_T, 0, 0, MPI_COMM_WORLD, &requests[1]);
+        }
+        MPI_Waitall(2, requests.data(), stats.data());
+    }
+    t = (MPI_Wtime() - t) / nruns;
     if (rank == 0) {
-        std::cout << msg_size << "," << max_time;
+        std::cout << msg_size / (1024*1024) << "," << t;
     }
     MPI_Finalize();
     return 0;
